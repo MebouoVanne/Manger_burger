e66acf34a2f4926b7e94ed2b050d8dfd15cbb785
MebouoVane <2017vaness@gmail.com>
2023-10-30 03:32:40 +0100

    ✨ F02: App>intall routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes,Route } from "react-router-dom";
+import LoginPage from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/order/OrderPage";
+import ErrorPage from "./components/pages/error/ErrorPage";
 
-function App() {
+// function App(){
+//   const [prenom, setPrenom] = useState("van")
+//   const [count, setCount] = useState(1)
+//   const handleClick = () =>{
+//     setCount(count +1);
+//   }
+
+//   return 
+//   <div>
+//       <h1>Bonjour {prenom}</h1>
+//       <p>{count}</p>
+//       <button onClick={handleClick}>increment</button>
+//   </div>
+  
+// }
+
+function App(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<LoginPage/>} />   
+      <Route path="/order" element={<OrderPage/>} />  
+      <Route path="*" element={<ErrorPage/>} /> 
+    </Routes>
+  )
+ 
+  
+  
 }
 
 export default App;
